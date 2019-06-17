4d5c5daf331e945311343f8ad91105e49f546b73
Sebastien Iooss <archimist.linux@gmail.com>
2019-06-18 00:58:58 +0200

    Front done

--- app.py
+++ app.py
@@ -1,40 +1,66 @@
 import os
 import threading
+import json
 from flask import Flask, url_for, redirect, Response, jsonify
+from absl import flags, app
+
+flags.DEFINE_string("images_path", "", "Path where are stored the images to annotate")
+flags.DEFINE_string("human_annotations_path", "", "Path where are stored human annotation")
+flags.DEFINE_string("model_annotations_path", "", "Path where are stored model annotation for helping human")
+
+FLAGS = flags.FLAGS
 
 image_provider = None
 
 
 class ImageProvider:
-    def __init__(self, img_dir: str, annotation_dir: str):
+    def __init__(self):
         """class providing path of images to process
-        
-        Args:
-            img_dir (str): dir containing the images
-            annotation_dir (str): dir containing the annotation files : one json per images
         """
-        self.img_dir = img_dir
-        self.image_list = os.listdir(self.img_dir)
-        self.annotation_dir = annotation_dir
+        # user-provided attributes
+        self.images_path = FLAGS.images_path
+        self.human_annotations_path = FLAGS.human_annotations_path
+        self.model_annotations_path = FLAGS.model_annotations_path
 
+        # other attributes
         self.lock = threading.Lock
         self.current_image = 0
 
-        # Create annotation dir if doesn't exist
-        os.makedirs(self.annotation_dir, exist_ok=True)
-        self.annotation_files = os.listdir(self.annotation_dir)
+        # Handle paths
+        assert os.path.exists(self.images_path)
+        os.makedirs(self.human_annotations_path, exist_ok=True)
+        if self.model_annotations_path:
+            os.makedirs(self.model_annotations_path, exist_ok=True)
 
-        if os.path.exists(processed_image_file):
-            for image in image_list:
-                if image.split("."):
-                    pass
-            # TODO continue
+        # Handle lists
+        self.images_list = os.listdir(self.images_path)
+        self.human_annotations_list = os.listdir(self.human_annotations_path)
+        if self.model_annotations_path:
+            self.model_annotations_list = os.listdir(self.model_annotations_path)
+        else:
+            self.model_annotations_list = []
 
+        # Only keep images that where not yet processed
+        new_list = []
+        human_annotation_ids = [os.path.splitext(file_name)[0] for file_name in self.human_annotations_list]
+        for image_name in self.images_list:
+            image_id, _ = os.path.splitext(image_name)
+            if image_id not in human_annotation_ids:
+                new_list.append(image_name)
+        self.images_list = new_list
+        
     def get_image(self):
         with self.lock:
-            image = self.image_list[self.current_image]
+            image = self.images_list[self.current_image]
             self.current_image += 1
-        return image
+
+        json_file_name = f"{os.path.splitext(image)[0]}.json"
+        if json_file_name in self.model_annotations_path:
+            with open(os.path.join(self.model_annotations_path, json_file_name), "r") as read_file:
+                data = json.load(read_file)
+            return image, data
+        else:
+            return image, {}
 
 
 app = Flask(__name__)
@@ -42,15 +68,16 @@ app = Flask(__name__)
 
 @app.route("/get_image")
 def get_image():
-    pass
+    global image_provider
+    return image_provider.get_image()
 
 
-@app.route("/set_image")
+@app.route("/set_image", methods=['POST'])
 def set_image():
     pass
 
 
-def main():
+def main(argv):
     global image_provider
     image_provider = ImageProvider()
     image_provider.start()
@@ -58,4 +85,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    app.run(main)
+    
--- front/src/App.vue
+++ front/src/App.vue
@@ -1,38 +1,85 @@
-<template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+<template lang="pug">
+  v-app(dark)
+    v-toolbar(app absolute clipped-left)
+      v-toolbar-side-icon(@click.stop="gui.nav_drawer_visible = !gui.nav_drawer_visible")
+      v-toolbar-title Dataset labeling
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    v-navigation-drawer(v-model="gui.nav_drawer_visible" absolute overflow app clipped)          
+      //- v-toolbar(flat)
+      //-   v-toolbar-side-icon(@click.stop="gui.nav_drawer_visible = !gui.nav_drawer_visible")
+      //-   v-toolbar-title Options
+        
+      v-list        
+        v-list-tile
+          v-btn(color="indigo" @click="update_list_of_rectangles()") Send
+          v-btn(color="indigo" @click="clear_list_of_rectangles()") Clear
+        
+    v-content
+      v-container(grid-list-md fluid)
+        v-layout(justify-space-around align-space-around)
+          v-flex(xs10)
+            rectangle(:href="image_src" drawable=true class_name="rectangles")
+
+    v-footer(app)
+      span(class="px-3") &copy; Sébastien IOOSS {{ new Date().getFullYear() }}
+        
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import rectangle from "./components/Rectangle";
+const axios = require("axios");
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    rectangle
   },
-  data () {
-    return {
-      //
+  methods: {
+    update_list_of_rectangles() {
+      let element = document.getElementsByClassName("rectangles");
+      let polylines = element[0].getElementsByTagName("polyline");
+
+      let points = [];
+      for (let polyline of polylines) {
+        points.push({
+          xMin: polyline.animatedPoints[0].x,
+          yMin: polyline.animatedPoints[0].y,
+          xMax: polyline.animatedPoints[2].x,
+          yMax: polyline.animatedPoints[2].y
+        });
+      }
+      this.rectangles = points;
+
+      axios.post("/set_image", { rectangles: this.rectangles });
+    },
+    clear_list_of_rectangles() {
+      let element = document.getElementsByClassName("rectangles");
+      let circles = element[0].getElementsByTagName("circle");
+      while (circles.length !== 0) {
+        circles[0].parentNode.removeChild(circles[0]);
+      }
+      let polylines = element[0].getElementsByTagName("polyline");
+      while (polylines.length !== 0) {
+        polylines[0].parentNode.removeChild(polylines[0]);
+      }
+
+      this.rectangles = [];
     }
+  },
+  data() {
+    return {
+      gui: {
+        nav_drawer_visible: true
+      },
+      image_src: "https://picsum.photos/200/300",
+      rectangles: []
+    };
+  },
+  mounted() {
+    axios.get("/get_image").then(function(response) {
+      // handle success
+      console.log(response);
+    });
   }
-}
+};
 </script>
