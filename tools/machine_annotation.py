d617f0ca1732e65f5c9435671f0f6b82b3850102
Jean-Baptiste Rouquier <jrouquie@example.com>
2019-07-01 15:07:52 +0200

    improve preannotation script, howto viz annotation

--- tools/machine_annotation.py
+++ tools/machine_annotation.py
@@ -6,17 +6,20 @@ import cv2
 import tensorflow as tf
 import numpy as np
 from absl import flags, app
-from tqdm import tqdm
+from tqdm import tqdm # progress bar
+
+# Run a frozen model on a set of images and output the detections as .json files, one per image.
+# For now it only keeps "detection_classes" == 1, i.e. "class"="person"
 
 coloredlogs.install(level="DEBUG")
 
-flags.DEFINE_string("model_path", None, "Path of the model to load and execute, for instance /mnt/nas/.../frozen_inference_graph.pb")
-flags.DEFINE_string("images_path", None, "Path where are stored the images to annotate")
-flags.DEFINE_string("model_annotations_path", None, "Path where are stored model annotation for helping human")
+flags.DEFINE_string("model_path", None, "Path of the model to load and execute, for instance models/frozen_inference_graph.pb")
+flags.DEFINE_string("input_dir",  None, "Path where the images to annotate are stored")
+flags.DEFINE_string("output_dir", None, "Path to store pre-annotations (model annotations to help human annotators)")
 
-flags.mark_flag_as_required("images_path")
-flags.mark_flag_as_required("images_path")
-flags.mark_flag_as_required("model_annotations_path")
+flags.mark_flag_as_required("model_path")
+flags.mark_flag_as_required("input_dir")
+flags.mark_flag_as_required("output_dir")
 
 FLAGS = flags.FLAGS
 
@@ -26,7 +29,7 @@ def main(argv):
     images_list = os.listdir(FLAGS.images_path)
     annotations_list = os.listdir(FLAGS.model_annotations_path)
 
-    # Only keep images that where not yet processed
+    # Only keep images that aren't processed yet
     new_list = []
     annotation_ids = [os.path.splitext(file_name)[0] for file_name in annotations_list]
     for image_name in images_list:
@@ -34,9 +37,9 @@ def main(argv):
         if image_id not in annotation_ids:
             new_list.append(image_name)
     images_list = new_list
-    logging.info(f"there are {len(images_list)} images to annotate")
+    logging.info("there are {} images to annotate".format(len(images_list)))
 
-    # load tensorflow model (need to be a frozen model)
+    # load tensorflow model (must be a frozen model)
     od_graph_def = tf.GraphDef()
     with tf.gfile.GFile(FLAGS.model_path, 'rb') as fid:
         serialized_graph = fid.read()
@@ -53,11 +56,11 @@ def main(argv):
                 tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
         image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-        # Now we can run inference
+        # Run inference
         first_iter = True
         for image_id in tqdm(range(len(images_list))):
-            image = cv2.cvtColor(cv2.imread(os.path.join(FLAGS.images_path, images_list[image_id])), cv2.COLOR_BGR2RGB)   
-            
+            image = cv2.cvtColor(cv2.imread(os.path.join(FLAGS.images_path, images_list[image_id])), cv2.COLOR_BGR2RGB)
+
             if first_iter:
                 print(image.shape)
                 first_iter=False
@@ -69,10 +72,13 @@ def main(argv):
             for i, detection_score in enumerate(output_dict["detection_scores"][0]):
                 if detection_score > 0.4:
                     box = output_dict["detection_boxes"][0][i]  # ymin, xmin, ymax, xmax
-                    good_rectangles.append({"xMin": int(box[1] * width),
-                                            "yMin": int(box[0] * height), 
-                                            "xMax": int(box[3] * width), 
-                                            "yMax": int(box[2] * height)})
+                    if output_dict["detection_classes"][0][i] == 1 \
+                       and box[3]-box[1] < 0.5  and  box[2]-box[0] <0.5 :
+                        good_rectangles.append({"xMin": int(box[1] * width),
+                                                "yMin": int(box[0] * height),
+                                                "xMax": int(box[3] * width),
+                                                "yMax": int(box[2] * height),
+                                                "class":"person"})
                 else:
                     break
 
